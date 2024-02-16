9675035d98ea48055826e705a50ffb508f38c1b7
KarstenSnapa <karsten.snapa@gmail.com>
2024-02-16 14:51:02 +0100

    .

---
+++ Main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+root = tk.Tk()
+root.title("Paint")
+
+button = tk.Button(root, text="Click Me!", command=button_click)
+
+# Place the button in the window
+button.pack()
+
+
+def chooseTool():
+    
+
+
+root.mainloop()
